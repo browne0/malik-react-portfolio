d63cee055bd6b8e6a5ce3e7dad6e720b924ac7fc
Malik Browne <mb516y@att.com>
2017-08-15 17:01:27 -0400

    Added the format for a blog link.
    Still needs to be responsive. Once that is done, we will move on to each blog post.

--- src/_posts/2017-08-10-hello-world.md
+++ src/_posts/2017-08-10-hello-world.md
@@ -1,5 +1,8 @@
 ---
-title: 'Hello world'
+title: Hello world
+author: Malik Browne
+twitter_author: https://twitter.com/milkstarz
+date_published: 2017-08-15
 ---
 
 # Hello world
@@ -9,3 +12,14 @@ function helloWorld() {
 	console.log('Hello world');
 }
 ```
+
+Lorem ipsum dolor sit amet, aenean proin, mi urna bibendum purus commodo vel justo. Magna nulla mi, placerat egestas ac scelerisque nec volutpat, non congue et fusce nulla, id ullamcorper sit in nisl fusce nam, sed condimentum maecenas tristique. Lectus ornare placerat diam. Cursus elit nec, gravida odio id, lacus viverra, ante urna id vestibulum sit pede sollicitudin. Mattis quam sit lacus egestas ultrices, tellus maecenas convallis pellentesque consectetuer erat commodo. Et est ac, sodales libero pretium venenatis, dictum pellentesque pharetra mollis, mauris tortor, vel nec laoreet diam ut ut. Condimentum viverra, nam accumsan laoreet. Aliquet nec integer nibh. Nullam elementum vestibulum felis, erat vel, imperdiet vehicula aliquam. Metus vitae nunc vitae mi laoreet, auctor elit, vivamus magna commodo. Massa nulla proin. Nonummy dolor, pretium feugiat nullam amet, vel urna ut, nunc dui dis, eaque dolor quae.
+
+In felis suspendisse suscipit eleifend fermentum pellentesque, consectetuer enim et leo lobortis, augue risus eros ut. Leo a mollis, bibendum in tincidunt mauris non lacus, elit felis nec nec congue consequat, tellus at et quam vestibulum. Quis quis, penatibus pellentesque dis phasellus eu. Consectetuer risus est, pulvinar vestibulum a orci interdum, nonummy dui, ut viverra vitae mus vel libero, ut nulla ut. Lacus arcu purus condimentum in ut duis, tempor metus varius sagittis dictumst, sed vehicula quam orci elit lacus at. Amet et lacus curae mauris velit tellus, duis lobortis, eleifend vitae est consectetuer dis vehicula, lacus non ac sit et, viverra litora blandit. A metus. Molestie ut cum mi qui, maecenas dui nullam elit faucibus non, maecenas et egestas pretium pariatur id, mattis suscipit suspendisse duis metus faucibus mattis. Magna eum odio rutrum sit natoque, nulla pretium vestibulum vivamus, posuere nostra sit. Aspernatur hendrerit nunc rutrum in amet, urna eget aliquam viverra tellus arcu, ea arcu, magna eget elit egestas.
+
+Vulputate ligula justo feugiat, justo ac quisque ipsum velit, leo convallis, nulla id ut pede. Nec pellentesque odio justo nec at mattis, diam consectetuer risus. Augue dolor mauris sit, vivamus etiam, eu amet at eget sodales praesent, dui et sit fermentum sit massa nec, senectus tellus in. Sollicitudin quis vestibulum fringilla lorem justo. Nam amet urna nec, a feugiat quis mauris vestibulum neque. Lobortis id. Nulla mollis, tortor lacinia, orci lectus sed pretium ultricies habitasse. Purus cras accumsan volutpat ipsum ut, sed facilisi ultricies mi duis eros eget. Nunc in aenean, vulputate sem. Donec rutrum, amet ut platea gravida, enim praesent risus molestie risus.
+
+Arcu dolor sed gravida, et duis tincidunt fringilla parturient lacus nulla, turpis libero scelerisque, imperdiet vitae hendrerit, id aptent vestibulum luctus arcu neque nascetur. Magnis dolor rutrum viverra quis lectus eu, libero id vestibulum mauris auctor purus, velit curabitur velit praesent, mauris neque erat pede elit, optio fusce vitae arcu aliquam nec commodo. Ante blandit, sunt maecenas neque. Ultrices vitae et wisi commodo, amet taciti ut duis. Mauris dui, dis ullamcorper fringilla aliquam est in non, et et placerat.
++
+
+
--- src/scenes/BlogList/index.js
+++ src/scenes/BlogList/index.js
@@ -1,28 +1,106 @@
-import React from 'react';
-import { Link, Route, Switch } from 'react-router-dom';
-import BlogPost from '../BlogPost';
+import React from "react";
+import { Link, Route, Switch } from "react-router-dom";
+import BlogPost from "../BlogPost";
+import Moment from "react-moment";
 
 // eslint-disable-next-line import/no-webpack-loader-syntax
-const webpackRequireContext = require.context('!markdown-with-front-matter-loader!../../_posts', false, /.md$/);
-const blogs = webpackRequireContext.keys().reduce((memo, fileName) => memo.set(fileName.match(/.\/([^.]+).*/)[1], webpackRequireContext(fileName)), new Map());
+const webpackRequireContext = require.context(
+  "!markdown-with-front-matter-loader!../../_posts",
+  false,
+  /.md$/
+);
+const blogs = webpackRequireContext
+  .keys()
+  .reduce(
+    (memo, fileName) =>
+      memo.set(
+        fileName.match(/.\/([^.]+).*/)[1],
+        webpackRequireContext(fileName)
+      ),
+    new Map()
+  );
+
+const blogRoutes = [...blogs.keys()].map(path =>
+  <Route
+    key={path}
+    path={"/blog/" + path}
+    component={BlogPost(blogs.get(path))}
+  />
+);
 
 console.log(blogs);
-const blogRoutes = [...blogs.keys()].map(path => <Route key={path} path={'/blog/' + path} component={BlogPost(blogs.get(path))} />);
 
-const blogList = (blogs) => ({match}) => 
-	<Switch>
-		<Route exact path={match.url} render={() => (
-			<div className="blog">
-				<ul>
-					{[...blogs.keys()].map(path => 
-						<li key={path}>
-							<Link to={`/blog/${path}`}>{blogs.get(path).title || path}</Link>
-							</li>
-					)}
-				</ul>
-			</div>
-		)}/>
-		{blogRoutes}
-	</Switch>
+const blogList = blogs => ({ match }) =>
+  <Switch>
+    <Route
+      exact
+      path={match.url}
+      render={() =>
+        <div className="blog">
+          <div>
+            {[...blogs.keys()].map(path =>
+              <Link key={path} to={`/blog/${path}`} className="post">
+                <img
+                  src="http://placekitten.com/36"
+                  alt=""
+                  className="avatar"
+                />
+
+                <a className="author" href={blogs.get(path).twitter_author}>
+                  {blogs.get(path).author}
+                </a>
+                <p className="date">
+                  <Moment parse="YYYY-MM-DD" format="MMM D">
+                    {blogs.get(path).date_published}
+                  </Moment>
+                  <span>&middot;</span>
+                  {Number(
+                    blogs
+                      .get(path)
+                      .__content.replace(/<[^>]*>/g, " ")
+                      .replace("/s+/g", " ")
+                      .trim()
+                      .split(" ").length
+                  ) /
+                    275 <
+                  1
+                    ? Number(
+                        String(
+                          Number(
+                            blogs
+                              .get(path)
+                              .__content.replace(/<[^>]*>/g, " ")
+                              .replace("/s+/g", " ")
+                              .trim()
+                              .split(" ").length
+                          ) /
+                            275 *
+                            60
+                        )
+                      )
+                        .toFixed()
+                        .toString() + " sec read"
+                    : Number(
+                        String(
+                          Number(
+                            blogs
+                              .get(path)
+                              .__content.replace(/<[^>]*>/g, " ")
+                              .replace("/s+/g", " ")
+                              .trim()
+                              .split(" ").length
+                          ) / 275
+                        )
+                      )
+                        .toFixed()
+                        .toString() + " min read"}
+                </p>
+              </Link>
+            )}
+          </div>
+        </div>}
+    />
+    {blogRoutes}
+  </Switch>;
 
 export default blogList(blogs);
