9b6c95899c8a1dd759135cb01a26d2d7eb90fab6
Malik Browne <browne0@purdue.edu>
2017-08-16 21:07:21 -0400

    FInished the hero for the about page.

--- src/scenes/About/index.js
+++ src/scenes/About/index.js
@@ -1,13 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class About extends Component {
   render() {
     return (
       <div className="About">
-        about
-			</div>
+        <div className="hero">
+          <div className="hero-text">
+            <h1>
+              Hi, I'm <span>Malik.</span>
+            </h1>
+            <h2>Currently a full stack engineer at AT&T.</h2>
+            <h2>Avid photographer, and YouTuber.</h2>
+          </div>
+        </div>
+      </div>
     );
   }
 }
 
-export default About
+export default About;
