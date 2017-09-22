722124002dcb92754ba9b6deb2516bd7abda8b9e
Malik Browne <browne0@purdue.edu>
2017-09-22 08:26:23 -0400

    Start work on the Spotter project section

--- src/scenes/Projects/Spotter/index.js
+++ src/scenes/Projects/Spotter/index.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import ProjectPage from '../../../components/ProjectPage';
-// import ProjectSection from '../../../components/ProjectSection';
+import React, { Component } from "react";
+import ProjectPage from "../../../components/ProjectPage";
+import ProjectSection from "../../../components/ProjectSection";
 
 class Spotter extends Component {
   constructor(props) {
@@ -8,9 +8,15 @@ class Spotter extends Component {
 
     this.state = {
       project: this.props.location.state
-    }
+    };
   }
   render() {
+    let style = {
+      title: {
+        color: this.state.project.background_color,
+        fontWeight: "bold"
+      }
+    };
     return (
       <ProjectPage
         name={this.state.project.name}
@@ -21,7 +27,27 @@ class Spotter extends Component {
         bgColor={this.state.project.background_color}
         technologies={this.state.project.technologies}
         images={this.state.project.image_urls.screenshots}
-        bigPicture={this.state.project.big_picture}>
+        bigPicture={this.state.project.big_picture}
+      >
+        <ProjectSection title="Background">
+          <p>
+            During my time in college, I helped create landing pages for several
+            different groups, businesses, and people. One of the more
+            interesting projects was the landing page for a fitness startup
+            called <span style={style.title}>Spotter.</span>
+          </p>
+
+          <p>
+            <span style={style.title}>Spotter</span> provides a fitness tracking
+            device that anyone from the everyday bodybuilder to the novice can
+            utilize fully.
+          </p>
+          <p>
+            Since the team had earned the opportunity to present at TechCrunch
+            in New York City, they needed a landing page for them to grow an
+            email list and show off their fully functional bluetooth fitness tracker.
+          </p>
+        </ProjectSection>
       </ProjectPage>
     );
   }
